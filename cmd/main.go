e345ff28e595f0ac636e459740f84a4fd64c676c
weqork <120187366+weqork@users.noreply.github.com>
2022-12-09 20:09:24 +0500

    simple messenger

--- cmd/main.go
+++ cmd/main.go
@@ -3,25 +3,24 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"net/http"
+	"messenger/internal"
 )
 
 func main() {
 	// Echo instance
 	e := echo.New()
-
+	k := internal.Messenger{}
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/", hello)
-
+	e.GET("/hello", k.Hello)
+	e.GET("/domoi", k.Dima)
+	e.POST("/msg", k.Message)
+	e.GET("/msg", k.History)
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 // Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
-}
