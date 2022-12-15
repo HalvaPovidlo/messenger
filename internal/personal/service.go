ab3fc6c96ac0797e75468dd9e5171b80042befef
Maksim Khodko <weqork@gmail.com>
2022-12-15 17:28:37 +0500

    Add personal messages sending

---
+++ internal/personal/service.go
@@ -0,0 +1,44 @@
+package personal
+
+import (
+	"errors"
+	"github.com/HalvaPovidlo/messenger/internal/pkg/message"
+)
+
+type key string
+
+type personal struct {
+	history map[key][]message.Item
+}
+
+func New() *personal {
+	history := make(map[key][]message.Item, 100)
+	return &personal{history: history}
+}
+
+func (k *personal) Message(from, to, msg string) error {
+	key := buildKey(from, to)
+	v := k.history[key]
+	if v == nil {
+		v = make([]message.Item, 0, 100)
+	}
+	v = append(v, message.Item{Name: from, Text: msg})
+	k.history[key] = v
+	return nil
+}
+
+func (k *personal) History(person1, person2 string) ([]message.Item, error) {
+	key := buildKey(person1, person2)
+	v := k.history[key]
+	if len(v) == 0 {
+		return nil, errors.New("no such dialogue")
+	}
+	return v, nil
+}
+
+func buildKey(from, to string) key {
+	if from > to {
+		return key(from + "_" + to)
+	}
+	return key(to + "_" + from)
+}
