3905863f6a6ec9286c0a674ea06ca5d3bef5a7b6
Maksim Khodko <weqork@gmail.com>
2022-12-22 00:42:21 +0500

    history of personal messages

--- internal/api/v1/messages/service.go
+++ internal/api/v1/messages/service.go
@@ -8,11 +8,13 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// внутренняя логика обработки сообщений
 type PersonalService interface {
 	Message(from, to, msg string) error
 	History(person1, person2 string) ([]message.Item, error)
 }
 
+// распаковщик http сообщений
 type Messenger struct {
 	personal      PersonalService
 	commonHistory []message.Item
@@ -32,10 +34,22 @@ func (m *Messenger) PersonalMessage(c echo.Context) error {
 	}
 	m.personal.Message(body.Name, name, body.Text)
 	fmt.Println(name)
-	return c.String(http.StatusOK, name)
+	return c.String(http.StatusOK, "")
 
 }
-
+func (m *Messenger) PersonalHistory(c echo.Context) error {
+	var otvet string
+	from := c.Param("from")
+	to := c.Param("to")
+	history, err := m.personal.History(from, to)
+	if err != nil {
+		return c.String(http.StatusNotFound, err.Error())
+	}
+	for i := 0; i < len(history); i++ {
+		otvet += history[i].Name + ": " + history[i].Text + "\n"
+	}
+	return c.String(http.StatusOK, otvet)
+}
 func (m *Messenger) History(c echo.Context) error {
 	var history string
 	for i := 0; i < len(m.commonHistory); i++ {
